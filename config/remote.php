fed71dcf56fef96142f434f6d8e04ebf4b6aa7f4
LuMiza <26994070434@qq.com>
2018-09-20 11:18:09 +0800

    modify remote.php

--- config/remote.php
+++ config/remote.php
@@ -3,10 +3,10 @@
 return [
     //ftp连接信息
     'ftp' => [
-        'host' => 'rumble.ftp-gz01.bcehost.com',
-        'port' => 8010,
-        'username' => 'rumble',
-        'password' => 'rumblemiza',
+        'host' => 'rumble.ftp-gz01.bcehost.com',//ftp地址
+        'port' => 8010,//端口
+        'username' => 'rumble',//用户名
+        'password' => 'rumblemiza',//密码
         'root_path'     => '/webroot/AAA',//文件存放的根目录【使用绝对地址】
     ],
     //文件将要存放那个目录下
@@ -15,5 +15,6 @@ return [
     'allow_file' => [
         'jpg', 'png', 'jpeg', 'txt', 'xlsx', 'xls', 'pdf', 'doc', 'docx', 'md'
     ],
+    //允许上传文件的最大size
     'size' => 10,//单位M
 ];
