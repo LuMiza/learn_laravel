a8683456144e00a446b26499dcd703aa9f63ee6b
LuMiza <26994070434@qq.com>
2018-09-20 11:22:49 +0800

    modify remote

--- config/remote.php
+++ config/remote.php
@@ -15,6 +15,6 @@ return [
     'allow_file' => [
         'jpg', 'png', 'jpeg', 'txt', 'xlsx', 'xls', 'pdf', 'doc', 'docx', 'md'
     ],
-    //允许上传文件的最大size
-    'size' => 10,//单位M
+    //允许上传文件的最大size 单位M
+    'size' => 10,
 ];
